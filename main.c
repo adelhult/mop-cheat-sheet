45145d63c5c00e1ac9a464330cfe92f68f9a1120
Eli Adelhult <hello@eli.nu>
2021-04-28 11:52:13 +0200

    Update main.c

--- main.c
+++ main.c
@@ -117,3 +117,29 @@ void delay_micro(unsigned int us) {
     us--;
   }
 }
+
+// ==== Graphics init ====
+
+__attribute__((naked))
+void graphic_initalize(void){
+    __asm volatile(" .HWORD 0xDFF0\n");
+    __asm volatile(" BX LR\n");
+}
+
+__attribute__((naked))
+void graphic_clear_screen(void){
+    __asm volatile(" .HWORD 0xDFF1\n");
+    __asm volatile(" BX LR\n");
+}
+
+__attribute__((naked))
+void graphic_pixel_set(int x, int y){
+    __asm volatile(" .HWORD 0xDFF2\n");
+    __asm volatile(" BX LR\n");
+}
+
+__attribute__((naked))
+void graphic_pixel_clear(int x, int y){
+    __asm volatile(" .HWORD 0xDFF3\n");
+    __asm volatile(" BX LR\n");
+}
